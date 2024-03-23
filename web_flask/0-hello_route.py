ced99e95e4c8a1071ff5f7596bbe5e062a30f94e
aelbouaa <hbouaami@gmail.com>
2024-03-23 21:02:31 +0000

    ?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
 """ script to practice flask """
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """retur string"""
+    """print  string"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)

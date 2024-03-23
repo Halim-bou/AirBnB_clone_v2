951d7a57e20177118bf8d390b46d3f15619d6c94
aelbouaa <hbouaami@gmail.com>
2024-03-23 21:19:23 +0000

    check

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script to practice flask """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """print  string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """print     hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """display    C    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+def python(text="is cool"):
+    """display    python   """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)

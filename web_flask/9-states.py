40267893343bdb7356af0dcfe95a2852ef0cf720
aelbouaa <hbouaami@gmail.com>
2024-03-27 22:11:44 +0000

    check this way

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ module doc """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
